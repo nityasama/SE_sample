788c02f41b5fca075c4245e1b7e6f7e7996465d8
nityasama <112641500+nityasama@users.noreply.github.com>
2022-10-30 11:05:16 -0500

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,138 +1,101 @@
-import { useState } from "react";
-import React from "react";
+import React from 'react';
+import './../App.css';
 import logo from './../images/logo.png';
-import '../../src/components/Registration.css';
-const Registration = () => {
-    const [newAccount, updatenewacc] = useState({
-      fname: "",
-      lname: "",
-      email: "",
-      dob: "",
-      ssn: "",
-      mobile: ""
-    });
-    const onSubmit = (e) => {
-      e.preventDefault();
-      let a = {
-        fname: e.target.fname.value,
-        lname: e.target.lname.value,
-        email: e.target.email.value,
-        dob: e.target.dob.value,
-        ssn: e.target.ssn.value,
-        mobile: e.target.mobile.value
-      };
-      updatenewacc(a);
-      console.log(newAccount);
-      console.log(JSON.stringify(newAccount));
-    };
-    return (
-      <form className="add-form" onSubmit={onSubmit}>
-         <img src={logo} style={{height: 100, width:100, float:'left' , margin:40}} alt="logo" />
-       
-        <div className="form-control">
+import '../../src/components/Login.css';
 
-       
-        <input
-            name="fname"
-            required={true}
-            type="text"
-            value={"First Name"}
-            style={{width:400, height:40, marginTop:150,textAlign:'center'}}
-            placeholder="First Name"
-            //value={text}
-            onChange={(e) => {
-              const value = e.target.value;
-              updatenewacc({ ...newAccount, fname: e.target.value });
-              //console.log(newAccount);
-            }}
-          />
-          <br />
-          
-          <input
-            name="lname"
-            required={true}
-            type="text"
-            value={"Last Name"}
-            style={{width:400, height:40, marginTop:50,textAlign:'center'}}
-            placeholder="Last Name"
-            //value={text}
-            onChange={(e) => {
-              const value = e.target.value;
-              updatenewacc({ ...newAccount, lname: e.target.value });
-              //console.log(newAccount);
-            }}
-          />
-          <br />
-          <br />
-          
-          <input
-            name="email"
-            required={true}
-            type="Email"
-            value={"Email"}
-            style={{width:400, height:40, marginTop:10,textAlign:'center'}}
-            placeholder="Email"
-            onChange={(e) => {
-              const value = e.target.value;
-              updatenewacc({ ...newAccount, email: e.target.value });
-            }}
-          />
-          <br />
-          <br />
-          
-          <input
-            name="dob"
-            required={true}
-            type="date"
-            value="DOB"
-            style={{width:400, height:40, marginTop:10,textAlign:'center'}}
-            //placeholder='First Name'
-            onChange={(e) => {
-              const value = e.target.value;
-              updatenewacc({ ...newAccount, dob: e.target.value });
-            }}
-          />
-          <br />
-          <br />
-         
-          <input
-            name="ssn"
-            required={true}
-            type="text"
-            value={"SSN"}
-            style={{width:400, height:40, marginTop:10,textAlign:'center'}}
-            placeholder="SSN"
-            onChange={(e) => {
-              const value = e.target.value;
-              updatenewacc({ ...newAccount, ssn: e.target.value });
-            }}
-          />
-          <br />
-          <br />
-          
-          <input
-            name="mobile"
-            required={true}
-            type="tel"
-            value={"Mobile"}
-            style={{width:400, height:40, marginTop:10,textAlign:'center'}}
-            placeholder="Mobile Number"
-            onChange={(e) => {
-              const value = e.target.value;
-              updatenewacc({ ...newAccount, mobile: e.target.value });
-            }}
-          />
+//this is not working yet
+const LoginUser =(e)=>{
+    e.preventDefault();
+    fetch("http://localhost:8080/login", {
+      method:"POST",
+      headers:{
+        "Content-Type" : "application/json"
+      },
+      //body: JSON.stringify(book)
+    })
+    .then(res=>{
+        console.log(1,res);
+        if(res.status === 201){
+          return res.json();
+        }else{
+          return null;
+        }
+      });
 
-          <br />
-           <input type="submit" value="Submit" className="btn"   style={{width:100, height:40, marginTop:10,textAlign:'center',color: 'blue',borderColor: 'blue'}} />
-        </div>
+  }
 
-        <h1 
-        style={{fontFamily:'"Trebuchet MS Bold Italic", "Trebuchet MS", sans-serif',fontWeight:700,fontStyle:'italic',fontSize:'25px', marginRight: 75, textAlign: 'center', marginTop:60}}>Create new Account</h1>
-       
-      </form>
+
+const Login = () => {
+  return (
+    //better way of writing flex items, need to check
+    /*<div className="Container">
+      <div class="fixed"><input type="text" 
+      value={"Username/Email"}  />
+      <br/>
+      <input type="text" 
+      value={"Password"}  />
+      <br/>
+      <input type="submit" value="Submit" /></div>
+  <div class="flex-item"><inputtext>
+      Test text...........
+      </inputtext></div>
+    </div>*/
+<div className='class'>
+    <div style={{width: '100%', overflow: 'hidden'}}>
+    <div style={{width: '600px', float: 'left'}}> 
+    <br/>
+    <br/>
+    <div className="Body">
+    <input  type="text" 
+      value={"Username/Email"} style={{width:200, height:40, marginTop:20}} />
+    </div>
+      <br/>
+      <br/>
+      <input className="Body" type="text" 
+      value={"Password"} style={{width:200, height:40, marginTop:10}} />
+      <br/>
+      <br/>
+      <input className="Body" type="submit" value="Login" style={{marginRight:100, width:100, height:40, marginTop:10, color: 'blue',borderColor: 'blue'}} onClick={LoginUser}/>
+      <br/>
+      <br/>
+      <div>
+      <inputtext style={{marginLeft: 40}}>
+      Not an existing user?
+     
+      </inputtext>
+      <br/>
+      {/* <input type="submit" style={{width:120, height:40, marginTop:10, marginRight:80,color: 'blue',borderColor: 'blue'}} value="Create Account"
+      <button>  
+      onClick={() => {
+      navigate("/Registartion");
+      </button>
+      }}/> */}
       
-    );
-  };
-  
-  export default Registration;
+      </div>
+      </div>
+    <img src={logo} style={{height: 100, width:100, float:'left' , margin:40}} alt="logo" />
+    
+    <h2 className='Title'>Fix My Fixture</h2>
+      
+    <h3 className='Caption'>
+      -Where booking made easy!
+    </h3>
+    <br></br>
+    <br></br>
+    <br></br>
+    <h3 classNAme='Description'>
+      
+      Get started with FmF by signing up and logging in to book appointments for all your needs. <br></br>
+      Select your service, location & slot and you will be booked with the appointment of your choice,<br></br>
+       making your day hassle free. Keep track of your booking details via email updates, so you do not miss any!
+
+    </h3>
+    
+      
+</div>
+</div>
+
+  )
+}
+
+export default Login
